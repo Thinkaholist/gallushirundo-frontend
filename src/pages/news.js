b141e71ab3974f5e091ce3d060b11e972061fc12
Richárd Németh <nemethricsi@gmail.com>
2021-07-25 10:52:06 +0200

    add react Masonry component

--- src/pages/news.js
+++ src/pages/news.js
@@ -2,13 +2,42 @@ import React from 'react';
 import { graphql, Link } from 'gatsby';
 import { DateTime } from 'luxon';
 import { ContainerStyles } from '../styles/ContainerStyles';
+import styled from 'styled-components';
+import Masonry from 'react-masonry-component';
+
+const Wrapper = styled.div`
+  --gap: 18px;
+  column-count: 3;
+  column-gap: var(--gap);
+`;
+
+const TestArticle = styled.article`
+  border: 1px solid;
+  margin-bottom: var(--gap);
+  padding: var(--gap);
+`;
 
 export default function NewsPage({ data }) {
   const news = data.news.nodes;
 
+  const articles = [
+    <TestArticle>6clscdlmcédlsmcdlsécmldésmclédsmc</TestArticle>,
+    <TestArticle>5</TestArticle>,
+    <TestArticle>4cdscdscds</TestArticle>,
+    <TestArticle>3cdsc</TestArticle>,
+    <TestArticle>2scdscdscdscdscdscds</TestArticle>,
+    <TestArticle>1</TestArticle>,
+  ];
+
   return (
     <>
       <ContainerStyles>
+        <h2>Normal Column Layout</h2>
+        <Wrapper>{articles.map((article) => article)}</Wrapper>
+        <hr />
+        <h2>React Masonry Component Layout</h2>
+        <Masonry>{articles}</Masonry>
+        <hr />
         <div style={{ columnCount: 3, columnGap: 18 }}>
           {news.map((post) => (
             <article
@@ -38,25 +67,6 @@ export default function NewsPage({ data }) {
             </article>
           ))}
         </div>
-        {/* <ul>
-          {data.allSanityPost.edges.map(({ node }) => (
-            <li key={node._id}>
-              <div>
-                <Link to={`/post/${node.slug.current}`}>{node.title}</Link>
-                <br />
-                <br />
-                <Link to={`/category/${node.category.slug.current}`}>
-                  {node.category.name}
-                </Link>
-                <p>
-                  {DateTime.fromISO(node.publishedDate).toFormat(
-                    'kkkk.LL.dd - T'
-                  )}
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul> */}
       </ContainerStyles>
     </>
   );
