09d90543f36206d188eee102b9bf7315da516c8a
Richárd Németh <nemethricsi@gmail.com>
2021-08-21 16:04:23 +0200

    add radiobuttons

--- src/pages/artists.js
+++ src/pages/artists.js
@@ -6,18 +6,93 @@ import { ContainerStyles } from '../styles/ContainerStyles';
 
 const GridWrapper = styled.div`
   display: grid;
+  gap: ${(p) => p.gap};
   grid-template-columns: repeat(auto-fill, minmax(min(300px, 100%), 1fr));
 `;
 
 export default function ArtistsPage({ data }) {
   const artists = data.allSanityArtist.nodes;
+  const [radius, setRadius] = React.useState('');
+  const [gap, setGap] = React.useState('');
 
   return (
     <>
       <ContainerStyles>
-        <GridWrapper>
+        <div
+          style={{
+            margin: '1rem auto',
+            padding: '1rem',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              padding: 10,
+            }}
+          >
+            <label>
+              <input
+                type='radio'
+                name='layout'
+                value=''
+                onClick={(e) => console.log(setRadius(e.target.value))}
+                defaultChecked
+              />
+              négyzet
+            </label>
+            <label>
+              <input
+                type='radio'
+                name='layout'
+                value='28px'
+                onClick={(e) => console.log(setRadius(e.target.value))}
+              />
+              28 px border-radius
+            </label>
+            <label>
+              <input
+                type='radio'
+                name='layout'
+                value='50%'
+                onClick={(e) => console.log(setRadius(e.target.value))}
+              />
+              kör
+            </label>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              padding: 10,
+            }}
+          >
+            <label>
+              <input
+                type='radio'
+                name='gap'
+                value=''
+                onClick={(e) => console.log(setGap(e.target.value))}
+                defaultChecked
+              />
+              No gap
+            </label>
+            <label>
+              <input
+                type='radio'
+                name='gap'
+                value='1rem'
+                onClick={(e) => console.log(setGap(e.target.value))}
+              />
+              16px gap
+            </label>
+          </div>
+        </div>
+        <GridWrapper gap={gap}>
           {artists.map((artist) => (
-            <ArtistCard key={artist._id} artist={artist} />
+            <ArtistCard key={artist._id} artist={artist} radius={radius} />
           ))}
         </GridWrapper>
       </ContainerStyles>
@@ -57,6 +132,8 @@ const ArtistCardStyles = styled.article`
   line-height: 1;
   position: relative;
   overflow: hidden;
+  transition: border-radius 0.25s;
+  border-radius: ${(p) => p.radius};
 
   h2 {
     color: var(--color-white);
@@ -86,7 +163,7 @@ const ArtistCardStyles = styled.article`
     }
     &:hover h2 {
       text-decoration: underline;
-      text-decoration-color: red;
+      text-decoration-color: var(--color-red, red);
     }
   }
 `;
@@ -98,13 +175,17 @@ const ImageStyles = styled(Img)`
   object-position: center center;
   transition: transform 0.35s ease-out;
   opacity: 0.9;
+
+  @supports not (aspect-ratio: 1 / 1) {
+    height: 300px;
+  }
 `;
 
-function ArtistCard({ artist }) {
+function ArtistCard({ artist, radius }) {
   return (
     <>
       <ArtisLink to={`/artist/${artist.slug.current}`}>
-        <ArtistCardStyles>
+        <ArtistCardStyles radius={radius}>
           <ImageStyles {...artist.featuredImage.image} alt={artist.name} />
           <h2>{artist.name}</h2>
         </ArtistCardStyles>
